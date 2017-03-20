d95635bbba6893d7a05bb0da8ef916bcd71df7d8
Shea Levy <shea@shealevy.com>
2017-03-20 15:43:35 -0400

    getLatest: Catch HttpExceptions and turn them into Left Strings

--- Hail/Hydra.hs
+++ Hail/Hydra.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Hail.Hydra where
 
+import Control.Exception (handle)
+
 import Network.Wreq
+import Network.HTTP.Client (HttpException)
 import Data.Aeson.Lens
 import Control.Lens.Operators
 import Data.Text (unpack)
@@ -11,14 +14,15 @@ getLatest
   :: String     -- ^ The URI to the hydra job.
   -> Maybe Auth -- ^ The credentials to talk to hydra.
   -> IO (Either String FilePath)
-getLatest uri m_creds = do
-    r <- getWith opts latestUri
-    let m_outPath = r ^? responseBody . key "buildoutputs"
-                                      . key "out"
-                                      . key "path" . _String
-    return $ case m_outPath of
-      Nothing -> Left noOutErr
-      Just outPath -> Right $ unpack outPath
+getLatest uri m_creds =
+    handle (return . Left . (show :: HttpException -> String)) $ do
+      r <- getWith opts latestUri
+      let m_outPath = r ^? responseBody . key "buildoutputs"
+                                        . key "out"
+                                        . key "path" . _String
+      return $ case m_outPath of
+        Nothing -> Left noOutErr
+        Just outPath -> Right $ unpack outPath
   where
     opts = defaults & header "Accept" .~ ["application/json"]
                     & auth .~ m_creds
